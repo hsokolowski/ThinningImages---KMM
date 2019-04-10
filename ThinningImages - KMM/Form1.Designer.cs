94d07ef61506358aa8e483d88ac91a6871bdf8dd
Hubert Sokołowski <hubert.sokool@outlook.com>
2019-04-10 17:26:03 +0200

    1 etam i 2 etap

--- ThinningImages - KMM/Form1.Designer.cs
+++ ThinningImages - KMM/Form1.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.loadImgBTN = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.binary = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.first = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(374, 385);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // loadImgBTN
+            // 
+            this.loadImgBTN.Location = new System.Drawing.Point(59, 40);
+            this.loadImgBTN.Name = "loadImgBTN";
+            this.loadImgBTN.Size = new System.Drawing.Size(75, 23);
+            this.loadImgBTN.TabIndex = 1;
+            this.loadImgBTN.Text = "Load";
+            this.loadImgBTN.UseVisualStyleBackColor = true;
+            this.loadImgBTN.Click += new System.EventHandler(this.loadImgBTN_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.loadImgBTN);
+            this.groupBox1.Location = new System.Drawing.Point(1069, 438);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Load file";
+            // 
+            // binary
+            // 
+            this.binary.Location = new System.Drawing.Point(1145, 364);
+            this.binary.Name = "binary";
+            this.binary.Size = new System.Drawing.Size(124, 23);
+            this.binary.TabIndex = 3;
+            this.binary.Text = "Binarization";
+            this.binary.UseVisualStyleBackColor = true;
+            this.binary.Click += new System.EventHandler(this.binary_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1145, 394);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(760, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(329, 385);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(407, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(329, 385);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // first
+            // 
+            this.first.Location = new System.Drawing.Point(895, 428);
+            this.first.Name = "first";
+            this.first.Size = new System.Drawing.Size(75, 23);
+            this.first.TabIndex = 8;
+            this.first.Text = "1Etap";
+            this.first.UseVisualStyleBackColor = true;
+            this.first.Click += new System.EventHandler(this.first_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1304, 550);
+            this.Controls.Add(this.first);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.binary);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button loadImgBTN;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button binary;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button first;
     }
 }
 
--- ThinningImages - KMM/Form1.cs
+++ ThinningImages - KMM/Form1.cs
@@ -16,5 +16,104 @@ namespace ThinningImages___KMM
         {
             InitializeComponent();
         }
+        Bitmap bmp;
+
+        private void loadImgBTN_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                bmp = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = bmp;
+            }
+            
+        }
+
+        private void binary_Click(object sender, EventArgs e)
+        {
+            Color color, new_color;
+            int r, g, b,a;
+            Bitmap t = new Bitmap(pictureBox1.Image);
+            Bitmap tmp = new Bitmap(t.Width, t.Height);
+
+            int manual = Convert.ToInt32(textBox1.Text);
+            if (manual > 255)
+            {
+                manual = 255;
+            }
+            else if (manual < 0)
+            {
+                manual = 0;
+            }
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    new_color = bmp.GetPixel(i, j);
+                    r = new_color.R;
+                    g = new_color.G;
+                    b = new_color.B;
+                    a = (r + g + b) / 3;
+
+                    tmp.SetPixel(i, j, a> manual ? Color.White : Color.Black);
+
+                }
+            }
+            pictureBox2.Image = tmp;
+            pictureBox2.Refresh();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void first_Click(object sender, EventArgs e)
+        {
+            Color color, black,white;
+            bool czyBok;
+            int r, g, b, a;
+            Bitmap t = new Bitmap(pictureBox2.Image); //zbinaryzowany
+            Bitmap tmp = new Bitmap(t.Width, t.Height);
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    color = t.GetPixel(i,j);
+                    if (color.R != 255)
+                    {
+                        tmp.SetPixel(i, j, kontur(i, j, t) ? Color.Red : Color.Black); //red (2) styka sie bokiem z tlem
+                        if(!kontur(i, j, t))
+                        {
+                            tmp.SetPixel(i, j, rog(i, j, t) ? Color.Green : Color.Black); //green stykaja się rogiem
+                        }
+                       
+                    }
+                    else tmp.SetPixel(i, j, Color.White);
+                    
+
+                }
+            }
+            pictureBox3.Image = tmp;
+            pictureBox3.Refresh();
+        }
+        private bool kontur(int x, int y,Bitmap tmp)
+        {
+            if (tmp.GetPixel(x - 1, y).R == 255) return true;
+            if (tmp.GetPixel(x , y-1).R == 255) return true;
+            if (tmp.GetPixel(x + 1, y).R == 255) return true;
+            if (tmp.GetPixel(x , y+1).R == 255) return true;
+
+            return false;
+        }
+        private bool rog(int x, int y, Bitmap tmp)
+        {
+            if (tmp.GetPixel(x - 1, y-1).R == 255) return true;
+            if (tmp.GetPixel(x-1, y + 1).R == 255) return true;
+            if (tmp.GetPixel(x + 1, y+1).R == 255) return true;
+            if (tmp.GetPixel(x+1, y - 1).R == 255) return true;
+
+            return false;
+        }
     }
 }
